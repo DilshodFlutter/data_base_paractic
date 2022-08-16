815b01cb659abaf9a43e9a97a43f8512d8d5f02d
dilshod_egamberdiyev <www.emacho900@gmail.com>
2022-08-16 18:21:14 +0500

    aksjsaka

---
+++ lib/src/ui/save_data.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SaveData extends StatefulWidget {
+  const SaveData({Key? key}) : super(key: key);
+
+  @override
+  State<SaveData> createState() => _SaveDataState();
+}
+
+class _SaveDataState extends State<SaveData> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: ListView(
+        children: [],
+      ),
+    );
+  }
+}
